c6b6523bedda9c4deec40e012c7e21cbfef26021
Aviral000 <avimalik02@gmail.com>
2024-08-29 10:34:32 +0530

    Some changes made for collecting info for database

--- backend/src/routes/apiRoutes.js
+++ backend/src/routes/apiRoutes.js
@@ -1,8 +1,10 @@
 const express = require('express');
-const { crawlAndRespond } = require('../controllers/apiController')
+const { crawlAndRespond } = require('../controllers/apiController');
+const { getAnalytics } = require('../controllers/analyticsController');
 
 const router = express.Router();
 
 router.post('/crawl', crawlAndRespond);
+router.get('/analytics', getAnalytics);
 
 module.exports = router;
